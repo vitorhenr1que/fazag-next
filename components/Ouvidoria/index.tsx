11681bfabe8413a6ed9ffe0437e7a61c5570f6e6
Vitor <vhpsantos@gmail.com>
2022-12-26 23:47:35 -0300

    adicionado api ao frontend

--- components/Ouvidoria/index.tsx
+++ components/Ouvidoria/index.tsx
@@ -1,13 +1,37 @@
-
 import { Cross2Icon } from "@radix-ui/react-icons";
 import * as Dialog from "@radix-ui/react-dialog";
 import styles from './style.module.scss'
 import * as ToggleGroup  from "@radix-ui/react-toggle-group";
-import { useState } from "react";
+import { FormEvent, useState } from "react";
+import { api } from "../../services/api";
+
+
 
 export function Ouvidoria() {
         const [procurouFAZAG, setProcurouFAZAG] = useState('sim')
         console.log(procurouFAZAG)
+
+        async function handleSubmit(e: FormEvent){
+          e.preventDefault()
+          const formData = new FormData(e.target as HTMLFormElement)
+          const data = Object.fromEntries(formData)
+          console.log(data.email)
+
+          try {
+            await api.post('/ouvidoria/create', {
+              nome: data.nome,
+              email: data.email,
+              motivo: data.motivo,
+              text: data.text,
+              vinculo: data.vinculo,
+              procurouSetor: procurouFAZAG
+            })
+
+            alert('Mensagem enviada!')
+          } catch(err){
+            console.log(err)
+          }
+        }
         
         return (
             <>
@@ -24,22 +48,22 @@ export function Ouvidoria() {
         <Dialog.Description className="DialogDescription">
           Ajuda a FAZAG a melhorar cada vez mais.
         </Dialog.Description>
-        <form action="" className={styles.form}>
+        <form onSubmit={handleSubmit} className={styles.form}>
           
-          <label htmlFor="name" className={styles.labels}>Nome *</label>
-          <input type="text" id="name" placeholder="Nome" className={`${styles.input}`} required/>
+          <label htmlFor="name" className={styles.labels}>Nome <span>*</span></label>
+          <input type="text" name="nome" id="name" placeholder="Nome" className={`${styles.input}`} required/>
           
          {/* <label htmlFor="cpf" className={styles.labels}>CPF (Opcional)</label>
           <input type="text" id="cpf" placeholder="CPF" className={`${styles.input}`} /> */}
 
-          <label htmlFor="email" className={styles.labels}>E-mail *</label>
-          <input type="text" id="email" placeholder="E-mail" className={`${styles.input}`} required/>
+          <label htmlFor="email" className={styles.labels}>E-mail <span>*</span></label>
+          <input type="text" name="email" id="email" placeholder="E-mail" className={`${styles.input}`} required/>
 
           <div className={styles.selectArea}>
 
           <fieldset>
-          <label htmlFor="vinculo" className={styles.labels}>Seu Vínculo *</label>
-          <select name="vinculo" id="vinculo" className={styles.select}>
+          <label htmlFor="vinculo" className={styles.labels}>Seu Vínculo <span>*</span></label>
+          <select name="vinculo" id="vinculo" className={styles.select} required>
             <option value="servidor">Servidor</option>
             <option value="aluno">Aluno</option>
             <option value="professor">Professor</option>
@@ -49,7 +73,7 @@ export function Ouvidoria() {
           </fieldset>
 
           <fieldset>
-          <label htmlFor="motivo" className={styles.labels}>Motivo *</label>
+          <label htmlFor="motivo" className={styles.labels}>Motivo <span>*</span></label>
           <select name="motivo" id="motivo" className={styles.select} required>
             <option value="critica">Crítica</option>
             <option value="denuncia">Denúncia</option>
@@ -63,21 +87,18 @@ export function Ouvidoria() {
           </div>
 
           <fieldset>
-          <label htmlFor="procurei" className={styles.labels}>Você procurou o setor envolvido na manifestação, antes de recorrer à Ouvidoria?</label>
+          <label htmlFor="procurei" className={styles.labels}>Você procurou o setor envolvido na manifestação, antes de recorrer à Ouvidoria? <span>*</span></label>
             <ToggleGroup.Root className={styles.toggleGroupRoot} type="single" defaultValue="sim" onValueChange={setProcurouFAZAG}>
               <ToggleGroup.Item value="sim" id="procurei" className={`${procurouFAZAG.includes('sim') ? styles.toggleGroupItemSelected : styles.toggleGroupItem} `} title="Sim">Sim</ToggleGroup.Item>
               <ToggleGroup.Item value="nao" className={`${procurouFAZAG.includes('nao') ? styles.toggleGroupItemSelected : styles.toggleGroupItem} `} title="Não">Não</ToggleGroup.Item>
             </ToggleGroup.Root>
           </fieldset>
 
-          <label htmlFor="text" className={styles.labels}>Utilize o quadro abaixo para relatar seu motivo de procura:</label>
-          <textarea name="text" id="text" rows={5} className={styles.textArea}></textarea>
+          <label htmlFor="text" className={styles.labels}>Utilize o quadro abaixo para relatar seu motivo de procura: <span>*</span></label>
+          <textarea name="text" id="text" rows={5} className={styles.textArea} required></textarea>
+          <button className={styles.enviarButton} type='submit'><span>Enviar</span></button>
         </form>
-        <div style={{ display: 'flex', marginTop: 25, justifyContent: 'flex-end' }}>
-          <Dialog.Close asChild>
-            <button className={styles.enviarButton}><span>Enviar</span></button>
-          </Dialog.Close>
-        </div>
+        
         <Dialog.Close asChild className={styles.close}>
           <button className={styles.IconButton} aria-label="Close">
             <Cross2Icon className={styles.crossIcon} />
