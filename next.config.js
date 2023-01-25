607804d3b0fb8f4fabe23b923efc8fbd7e3da19e
Vitor <vhpsantos@gmail.com>
2023-01-25 10:32:26 -0300

    adicionado rota para todos os posts

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  async rewrites() {
+    return [
+      {
+        source: '/api/posts/allposts',
+        destination: 'https://fazag-next.vercel.app/api/posts/allposts',
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
