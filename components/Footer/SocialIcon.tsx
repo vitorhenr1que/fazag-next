108f6fb34014875c9d601d27055c603a3171c0c4
Vitor <vhpsantos@gmail.com>
2023-02-25 13:41:55 -0300

    footer reformulado

---
+++ components/Footer/SocialIcon.tsx
@@ -0,0 +1,13 @@
+import styles from './style.module.scss'
+
+interface SocialIconProps{
+    buttonColor: string;
+    logo: any;
+    link: string;
+}
+
+export function SocialIcon({buttonColor, logo, link}: SocialIconProps){
+    return (
+        <a className={styles.icon} style={{background: `${buttonColor}`}} href={link}>{logo}</a>
+    )
+}
