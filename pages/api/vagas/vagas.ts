aca134ad6b9582910d02a34846ba864d671ae6fb
Vitor <vhpsantos@gmail.com>
2023-09-30 09:05:01 -0300

    Alterações na api vagas/vagas

--- pages/api/vagas/vagas.ts
+++ pages/api/vagas/vagas.ts
@@ -2,14 +2,15 @@ import { prisma } from "../../../services/prisma";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function (req: NextApiRequest, res: NextApiResponse){
-    const  course  = req.body
-
+    const  {course}  = req.body
+    console.log(course)
     const getNumberVencace = await prisma.vagas.findUnique({
         where: {
             id: 'b90b0686-f9fa-4d3f-a8f9-35c5223346fa'
         },
         select: course
     })
+
 
     return res.status(200).json({getNumberVencace})
 }
