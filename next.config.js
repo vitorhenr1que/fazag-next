5d2e5abb231d7075f18a5d9e387e93df906c4f91
Vitor <vhpsantos@gmail.com>
2023-09-29 22:17:10 -0300

    alteração next config1

--- next.config.js
+++ next.config.js
@@ -26,7 +26,7 @@ const nextConfig = {
             source: "/api/:path*",
             headers: [
                 { key: "Access-Control-Allow-Credentials", value: "true" },
-                { key: "Access-Control-Allow-Origin", value: "https://matriculas.fazag.edu.br" }, // Colocar site específico que pode fazer requisições
+                { key: "Access-Control-Allow-Origin", value: "*" }, // Colocar site específico que pode fazer requisições https://matriculas.fazag.edu.br
                 { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT,OPTIONS" },
                 { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
                 { key: "Access-Control-Max-Age", value: "86400" },
