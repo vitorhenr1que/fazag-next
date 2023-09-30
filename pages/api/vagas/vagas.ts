ecdc6fcfe109d098c5d469d1a7a44cf1456c42c7
Vitor <vhpsantos@gmail.com>
2023-09-30 00:05:23 -0300

    Test

--- pages/api/vagas/vagas.ts
+++ pages/api/vagas/vagas.ts
@@ -2,13 +2,13 @@ import { prisma } from "../../../services/prisma";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function (req: NextApiRequest, res: NextApiResponse){
-    const data = req.body
+    const { course } = req.body
 
     const getNumberVencace = await prisma.vagas.findUnique({
         where: {
             id: 'b90b0686-f9fa-4d3f-a8f9-35c5223346fa'
         },
-        select: data
+        select: course
     })
 
     return res.status(200).json({getNumberVencace})
