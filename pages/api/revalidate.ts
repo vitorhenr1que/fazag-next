3b9f04fe2ea4df4465d3d1623daf16e7b58bf123
Vitor Henrique <vhpsantos@gmail.com>
2023-11-24 11:52:11 -0300

    atualização dos cursos 2

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -13,7 +13,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       return res.json({ revalidated: true })
     } catch (err) {
       // If there was an error, Next.js will continue
-      // to show the last successfully generated pag
+      // to show the last successfully generated page
       return res.status(500).send('Error revalidating')
     }
   }
