b3796f2cf603e82ad9d2bf8a4d99b403b02ca7e8
Vitor Henrique <vhpsantos@gmail.com>
2024-01-09 16:24:52 -0300

    Alterações no curso de educação física

--- pages/test.tsx
+++ pages/test.tsx
@@ -3,11 +3,10 @@ import { getClient } from "../services/prismic"
 import * as prismic from '@prismicio/client' 
 import { Modal } from "../components/Home/Modal"
 
-export default function Test({imageModal}: any){
+export default function Test({linkImgModal, altImgModal, modalLink}: any){
     return(
         <>
-        {console.log(imageModal)}
-        <Modal image={imageModal}/>
+        <Modal image={linkImgModal} alt={altImgModal} modalLink={modalLink}/>
         </>
     )
 }
@@ -15,16 +14,24 @@ export default function Test({imageModal}: any){
 
 
 export const getServerSideProps: GetServerSideProps = async () => {
- const response = await getClient().getByUID('images', 'modal', {})
- const responseTwo = await getClient().getByType('posts', {
-  predicates: [
 
-  ]
- })
-const imageModal = response.data.image
- return {
- props: {
-    imageModal: imageModal.url
- }
- }
+        const response = await getClient().getByUID('images', 'modal', {})
+        const responseTwo = await getClient().getByType('posts', {
+         predicates: [
+       
+         ]
+        })
+       const imageModal = response.data.image
+       const linkImgModal = response.data.image.url
+       const altImgModal = response.data.image.alt
+       const modalLink = response.data.link
+        return {
+        props: {
+            linkImgModal,
+            altImgModal,
+            modalLink
+        }
+        }
+    
+
 }
