e160550b0775c196e8410dfd6c57ee1bca5d5155
Vitor Henrique <vitor@MacBook-Air-de-Vitor.local>
2025-08-11 16:05:48 -0300

    alteração no service api

--- pages/api/nusp/find.ts
+++ pages/api/nusp/find.ts
@@ -7,7 +7,7 @@ export default async function (req: NextApiRequest, res: NextApiResponse){
 
     const data: any = req.body
     console.log('ESSE É A DATA: ', data)
-
+     
 if(req.method === 'POST'){
     const findNusp = await prisma.nusp.findMany(
         {
