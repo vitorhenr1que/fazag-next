f1632655d002cafaf70793fe34eb72f2a13442f0
Vitor Henrique <vitor@MacBook-Air-de-Vitor.local>
2025-08-15 16:08:36 -0300

    adicionado pic ao site

--- pages/publicacoes-institucionais/index.tsx
+++ pages/publicacoes-institucionais/index.tsx
@@ -1,6 +1,7 @@
 import { PublicacaoInstitucional } from '../../components/PublicacaoInstitucional/PublicacaoInstitucional'
 import styles from '../../styles/publicInstitucionais.module.scss'
 
+
 export default function PublicacoesInstitucionais(){
     const pdfSvg = 'M14.208 21.917h1.584V18.5h1.875q.666 0 1.125-.458.458-.459.458-1.125v-1.875q0-.667-.458-1.125-.459-.459-1.125-.459h-3.459Zm1.584-5v-1.875h1.875v1.875Zm5.25 5h3.416q.667 0 1.125-.459.459-.458.459-1.125v-5.291q0-.667-.459-1.125-.458-.459-1.125-.459h-3.416Zm1.583-1.584v-5.291h1.833v5.291Zm5.333 1.584h1.584V18.5H31.5v-1.583h-1.958v-1.875H31.5v-1.584h-3.542Zm-16.291 9.208q-1.125 0-1.959-.833-.833-.834-.833-1.959V6.125q0-1.125.833-1.958.834-.834 1.959-.834h22.208q1.125 0 1.958.834.834.833.834 1.958v22.208q0 1.125-.834 1.959-.833.833-1.958.833Zm0-2.792h22.208V6.125H11.667v22.208Zm-5.542 8.334q-1.125 0-1.958-.834-.834-.833-.834-1.958v-25h2.792v25h25v2.792Zm5.542-30.542v22.208V6.125Z'
 return (
@@ -81,6 +82,7 @@ return (
             <div className="collapse" id={`logo-institucional`}>
             <div className={`card card-body ${styles.cardBody}`}>
         <PublicacaoInstitucional 
+            type='image'
             viewBox='0 0 15 15'
             downloadName='FAZAG Logo Azul'
             destination='static/logo/logo-fazag-azul.png' 
@@ -89,6 +91,7 @@ return (
             />
             <hr/>
         <PublicacaoInstitucional 
+            type='image'
             viewBox='0 0 15 15'
             downloadName='FAZAG Logo Branca'
             destination='static/logo/logo-fazag-branca.png' 
@@ -109,7 +112,7 @@ return (
             <div className="collapse" id={`iniciacao-cientifica`}>
             <div className={`card card-body ${styles.cardBody}`}>
         <PublicacaoInstitucional 
-
+            type='pdf'
             downloadName='Edital PIC'
             destination='static/pic/editalpic.pdf' 
             label='Edital'
@@ -117,11 +120,10 @@ return (
             />
             <hr />
             <PublicacaoInstitucional 
-
-            downloadName='Formulário de Inscrição - PIC'
-            destination='static/pic/formalunopic.pdf' 
+            type='link'
+            destination='https://docs.google.com/forms/d/e/1FAIpQLSc10nNUaZqhRVn7t6Kh73ChnnxZ4EcdInD52b3Wu6S-5rfQ0A/viewform?usp=header' 
             label='Formulário'
-            svgPath={pdfSvg}
+            svgPath={'../../public/images/icons/icon-link.png'}
             />
             </div>
             </div>
