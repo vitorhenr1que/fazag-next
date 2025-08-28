4a649986264c8da94b12cebdfef98f85a03f3038
Vitor Henrique <vitor@MacBook-Air-de-Vitor.local>
2025-08-28 14:08:20 -0300

    teste 2

--- pages/api/prismic/webhook.ts
+++ pages/api/prismic/webhook.ts
@@ -99,9 +99,9 @@ export default async function handler(
     return;
   }
 
-  console.log(`Response: -> ${res}`)
-  console.log(`Request: -> ${req}`)
-  
+  console.log(`Response: -> ${JSON.stringify(res)}`)
+  console.log(`Request: -> ${JSON.stringify(req)}`)
+
   // 1) valida secret
   const secret = getSecret(req);
   if (!secret || secret !== process.env.PRISMIC_ACCESS_TOKEN) {
