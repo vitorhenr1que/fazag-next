22fb4c8b1bc7a29cd93e279e1aa2b996745d81e1
Vitor Henrique <vitor@MacBook-Air-de-Vitor.local>
2025-11-17 15:24:35 -0300

    alterações do link do sistema

--- components/Home/HomeMain.tsx
+++ components/Home/HomeMain.tsx
@@ -38,7 +38,7 @@ export default function HomeMain({posts}: PostsProps) {
           src={AmbienteVirtual}/>
           
           <ButtonsMain 
-          url="https://fazag.sistemajaguar.com.br"
+          url="https://portais.qualinfonet.com.br/fazag/"
           title="Portal do Aluno" 
           description="Acesse suas notas, comprovante de matrícula, certificados e muito mais."
           src={PortalAluno}/>
